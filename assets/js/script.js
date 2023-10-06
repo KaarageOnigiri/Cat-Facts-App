18badb0231e1ae2b5c0401077b3091280f99e289
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-10-05 20:43:22 -0400

    sound added for button

--- assets/js/script.js
+++ assets/js/script.js
@@ -586,4 +586,22 @@ async function fetchPreviousBreedFacts(){
 }
 // END: same as the function above
 
+//Cat sound button 
+ 
+const emitSound = () => {
+    var Catfight = "assets/cat_sounds/Cat-fight.mp3";
+    var Catgrowl = "assets/cat_sounds/Cat-growl.mp3";
+    var Catpurr = "assets/cat_sounds/Cat-purr.mp3";
+    var kittenmeow = "assets/cat_sounds/kitten-meow.mp3";
+
+    const animalsound = new Audio(kittenmeow);
+    animalsound.load();
+    animalsound.play();
+
+    console.log('test sound -> ', animalsound);
+}
+
+var speakerBtn = document.getElementById("animal-sound")
+speakerBtn.addEventListener("click", emitSound);
+
 
